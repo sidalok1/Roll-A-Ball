f4c470b0c6f55a543ec54ca3d9017c7071d2b301
Sidharth Alok Girdhar <116860545+sidalok1@users.noreply.github.com>
2023-10-13 11:15:48 -0700

    Added Jumping and Respawn
    +Added Jumping Mechanic with jumping limited to when on (or very close to) the ground.
    +Added world floor that respawns player when hit
    +Began adding possible code for moveable camera
    -Small formatting changes

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
+    private Vector2 mDelta;
     void Start()
     {
         offset = transform.position - player.transform.position; 
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
+        mDelta = Mouse.current.delta.ReadValue()*Time.smoothDeltaTime;
+        Debug.Log(mDelta);
         transform.position = player.transform.position + offset; 
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,9 +9,13 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb; 
     private float movementX;
     private float movementY;
+    [SerializeField] private float jumpVal = 5;
+    private bool onGround = true;
+    private Vector3 spawn;
     void Start()
     {
-        rb = GetComponent <Rigidbody>() ; 
+        rb = GetComponent<Rigidbody>(); 
+        spawn = rb.position;
     }
 
     private void FixedUpdate() 
@@ -27,15 +31,44 @@ public class PlayerController : MonoBehaviour
      {
           other.gameObject.SetActive(false);
      }
+     if (other.gameObject.CompareTag("GND")) {
+          onGround = true;
+          Debug.Log("Ground");
+     }
+     if (other.gameObject.CompareTag("Respawn")) {
+          rb.position = spawn;
+          rb.velocity = Vector3.zero;
+          Debug.Log("respawning");
+     }
+   }
+
+   void OnTriggerExit (Collider other) {
+     if (other.gameObject.CompareTag("GND")) {
+          onGround = false;
+          Debug.Log("Not Ground");
+     }
+     if (other.gameObject.CompareTag("Respawn")) {
+          rb.velocity = Vector3.zero;
+          Debug.Log("respawn");
+     }
    }
 
     void OnMove (InputValue movementValue)
-   {
-        Vector2 movementVector = movementValue.Get<Vector2>(); 
-        movementX = movementVector.x; 
-        movementY = movementVector.y; 
+   { 
+        Vector2 v = movementValue.Get<Vector2>();
+        movementX = v.x; 
+        movementY = v.y; 
    }
 
-    
+    void OnJump () {
+     
+     if (onGround == true) {
+          rb.velocity += new Vector3(0, jumpVal, 0);
+          Debug.Log("Jumped");
+     } else if (onGround == false) {
+          Debug.Log("Off the ground!!!");
+     }
+     
+    }
     
 }
